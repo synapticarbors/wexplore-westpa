ff7832f0484ccd170b26fe055444b4c90a5eee35
Joshua Adelman <joshua.adelman@gmail.com>
2014-12-22 11:49:21 -0500

    Fixed DicksonRingPotential binspace visualization in the ipynb

--- examples/DicksonRingPotential/system.py
+++ examples/DicksonRingPotential/system.py
@@ -52,8 +52,8 @@ class System(WESTSystem):
         self.pcoord_dtype = pcoord_dtype
 
         # Set up hierarchical bin space
-        self.bin_mapper = wexplore.WExploreBinMapper(n_regions=[4, 4, 4],
-                d_cut=[4.1, 2, 0.5], dfunc=modelsim.dfunc)
+        self.bin_mapper = wexplore.WExploreBinMapper(n_regions=[4, 6, 18],
+                d_cut=[4.0, 1.5, 0.3], dfunc=modelsim.dfunc)
         
 
         # Array to hold bin centers
@@ -61,7 +61,7 @@ class System(WESTSystem):
         self.bin_mapper.add_bin(None, 0)
 
         # Need to define the max number of replicas
-        self.max_replicas = 640
+        self.max_replicas = 600
         self.bin_target_counts = self.bin_mapper.balance_replicas(self.max_replicas,
                 np.array([0,], np.int_))
 
